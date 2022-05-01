d3c3d35fbf8020bc75caa3f49c7ac8f35a16fb50
slimfetz <trentonfetzer_6@hotmail.com>
2022-04-30 21:33:37 -0600

    Update iotBoard.cpp

--- src/iotBoard.cpp
+++ src/iotBoard.cpp
@@ -4,18 +4,7 @@
 
 iotBoard::iotBoard(string boardType){
   
-  if (boardType == "arduino"){
-    pinMode(2,OUTPUT);
-    pinMode(3,OUTPUT);
-    pinMode(4,OUTPUT);
-    pinMode(5,OUTPUT);
-    pinMode(6,OUTPUT);
-    pinMode(7,OUTPUT);
-    pinMode(8,OUTPUT);
-    pinMode(9,OUTPUT);
-  }
-  
-  if (boardType == "particle"){
+  if (boardType == "arduinoMKR"){
     pinMode(D0,OUTPUT);
     pinMode(D1,OUTPUT);
     pinMode(D2,OUTPUT);
@@ -24,9 +13,66 @@ iotBoard::iotBoard(string boardType){
     pinMode(D5,OUTPUT);
     pinMode(D6,OUTPUT);
     pinMode(D7,OUTPUT);
-    pinMode(D8,OUTPUT);
-    pinMode(D9,OUTPUT);
+    
+    pinMode(A0,INPUT);
+    pinMode(A1,INPUT);
+    pinMode(A2,INPUT);
+    pinMode(A4,INPUT);
+    pinMode(A5,INPUT);
+    pinMode(A6,INPUT);
+    pinMode(8,INPUT);
+    pinMode(9,INPUT);
+    pinMode(10,INPUT);
+    pinMode(11,INPUT);
   }
+  
+  if (boardType == "arduinoNano33"){
+    pinMode(2,OUTPUT);
+    pinMode(3,OUTPUT);
+    pinMode(4,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(7,OUTPUT);
+    pinMode(8,OUTPUT);
+    pinMode(9,OUTPUT);
+    
+    pinMode(A0,INPUT);
+    pinMode(A1,INPUT);
+    pinMode(A2,INPUT);
+    pinMode(A4,INPUT);
+    pinMode(A5,INPUT);
+    pinMode(A6,INPUT);
+    pinMode(A7,INPUT);
+    pinMode(10,INPUT);
+    pinMode(11,INPUT);
+    pinMode(12,INPUT);
+  }
+  
+  if (boardType == "particlePhoton" || boardType == "particleBoron || boardType == "particleElectron){
+    pinMode(D0,OUTPUT);
+    pinMode(D1,OUTPUT);
+    pinMode(D2,OUTPUT);
+    pinMode(D3,OUTPUT);
+    pinMode(D4,OUTPUT);
+    pinMode(D5,OUTPUT);
+    pinMode(D6,OUTPUT);
+    pinMode(D7,OUTPUT);
+    
+    pinMode(A0,INPUT);
+    pinMode(A1,INPUT);
+    pinMode(A2,INPUT);
+    pinMode(A3,INPUT);
+    pinMode(A4,INPUT);
+    pinMode(A5,INPUT);
+    pinMode(A6,INPUT);
+    pinMode(A7,INPUT);
+  }
+  
+  if (boardType == "particleBoron){
+    pinMode(D8,INPUT);
+  }  
 }
 
-bool input(int )
+bool iotBoard::inputDigital(int pinIn, string inputType){
+    pinMode(pinIn, INPUT);
+}
