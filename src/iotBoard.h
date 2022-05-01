aaddcd909c4316fc7a1cede5662e9728cf396919
slimfetz <trentonfetzer_6@hotmail.com>
2022-05-01 10:13:19 -0600

    Update iotBoard.h

--- src/iotBoard.h
+++ src/iotBoard.h
@@ -1,12 +1,10 @@
 #ifndef iotBoard_h
 #define iotBoard_h
 
-#include "Particle.h"
-#include "Arduino.h"
-
 class iotBoard{
   public:
     iotBoard(string boardType);
+    string _boardType;
     bool input(int pinIn, string inputType);
     void output(int pinOut);
   private:
