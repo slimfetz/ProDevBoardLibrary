96fd8a27456190183c4989d10acf0c1e8bf4ff27
slimfetz <trentonfetzer_6@hotmail.com>
2022-05-01 19:33:17 -0600

    Update IotBoardParticle.cpp

--- src/IotBoardParticle.cpp
+++ src/IotBoardParticle.cpp
@@ -1,21 +1,25 @@
+#include "Arduino.h"
+#include "iotBoardParticle.h"
 
-  if (boardType == "particlePhoton" || boardType == "particleBoron || boardType == "particleElectron"){
-    pinMode(D0,OUTPUT);
-    pinMode(D1,OUTPUT);
-    pinMode(D2,OUTPUT);
-    pinMode(D3,OUTPUT);
-    pinMode(D4,OUTPUT);
-    pinMode(D5,OUTPUT);
-    pinMode(D6,OUTPUT);
-    pinMode(D7,OUTPUT);
+iotBoard::iotBoard(string boardType){
+  
+  _boardType = boardType;
+  
+  pinMode(D0,OUTPUT);
+  pinMode(D1,OUTPUT);
+  pinMode(D2,OUTPUT);
+  pinMode(D3,OUTPUT);
+  pinMode(D4,OUTPUT);
+  pinMode(D5,OUTPUT);
+  pinMode(D6,OUTPUT);
+  pinMode(D7,OUTPUT);
     
-    pinMode(A0,INPUT);
-    pinMode(A1,INPUT);
-    pinMode(A2,INPUT);
-    pinMode(A3,INPUT);
-    pinMode(A4,INPUT);
-    pinMode(A5,INPUT);
-  }
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A3,INPUT);
+  pinMode(A4,INPUT);
+  pinMode(A5,INPUT);
   
   if (boardType == "particlePhoton" boardType == "particleElectron){
     pinMode(A6,INPUT);
