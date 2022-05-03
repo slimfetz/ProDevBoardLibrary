937c91aec30138d1e9cd885e30cffddf8f67f050
slimfetz <trentonfetzer_6@hotmail.com>
2022-05-03 17:36:19 -0600

    Update ArduinoMKRPinout.h

--- src/ArduinoMKRPinout.h
+++ src/ArduinoMKRPinout.h
@@ -1,1 +1,16 @@
+  
 
+
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A4,INPUT);
+  pinMode(A5,INPUT);
+  pinMode(A6,INPUT);
