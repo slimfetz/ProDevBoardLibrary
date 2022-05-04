a5f6c1c7d29baa9b75e6ce07c975a637efec5d94
slimfetz <trentonfetzer_6@hotmail.com>
2022-05-03 21:21:50 -0600

    Update iotBoard.cpp

--- src/iotBoard.cpp
+++ src/iotBoard.cpp
@@ -5,38 +5,11 @@ iotBoard::iotBoard(String boardType){
   
   _boardType = boardType;
   
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
-  
-  pinMode(A0,INPUT);
-  pinMode(A1,INPUT);
-  pinMode(A2,INPUT);
-  pinMode(A4,INPUT);
-  pinMode(A5,INPUT);
-  pinMode(A6,INPUT);
-  
-  if (boardType == "arduinoMKR"){
-    pinMode(0,OUTPUT);
-    pinMode(1,OUTPUT);
-
-    pinMode(8,INPUT);
-    pinMode(9,INPUT);
-    pinMode(10,INPUT);
-    pinMode(11,INPUT);
+  if (boardType == "ArduinoMKR"){
+    #include "ArduinoMKRSetup.h"
   }
-  
-  if (boardType == "arduinoNano33"){
-    pinMode(8,OUTPUT);
-    pinMode(9,OUTPUT);
-    
-    pinMode(A7,INPUT);
-    pinMode(10,INPUT);
-    pinMode(11,INPUT);
-    pinMode(12,INPUT);
+  else if (boardType == "arduinoNano33"){
+    #include "ArduinoNano33Setup.h"
   }
 }
 
