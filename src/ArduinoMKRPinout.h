3137f0a02190568ed06a594e614697285a41f2f2
slimfetz <trentonfetzer_6@hotmail.com>
2022-05-03 21:29:03 -0600

    Update ArduinoMKRPinout.h

--- src/ArduinoMKRPinout.h
+++ src/ArduinoMKRPinout.h
@@ -1,19 +1,18 @@
-#ifndef ArduinoMKRPinout_h
-#define ArduinoMKRPinout_h
+#ifndef ArduinoMKRSetup_h
+#define ArduinoMKRSetup_h
 
 #include "Arduino.h"
 
   _input1 = A0;
   _input2 = A1;
-  _input1 = A2;
-  _input1 = A3;
-  _input1 = A4;
-  _input1 = A5;
-  _input1 = A6;
-  _input1 = 8;
-  _input1 = 9;
-  _input1 = 10;
-  _input1 = 11:
+  _input3 = A2;
+  _input4 = A3;
+  _input5 = A4;
+  _input6 = A5;
+  _input7 = A6;
+  _input8 = XX;
+  _input9 = XX;
+  _input10 = XX;
   
   _output1 = 0;
   _output2 = 1;
@@ -23,5 +22,25 @@
   _output6 = 5;
   _output7 = 6;
   _output8 = 7;
+
+  pinMode(_input1, INPUT);
+  pinMode(_input2, INPUT);
+  pinMode(_input3, INPUT);
+  pinMode(_input4, INPUT);
+  pinMode(_input5, INPUT);
+  pinMode(_input6, INPUT);
+  pinMode(_input7, INPUT);
+  pinMode(_input8, INPUT);
+  pinMode(_input9, INPUT);
+  pinMode(_input10, INPUT);
+
+  pinMode(_output1, OUTPUT);
+  pinMode(_output2, OUTPUT);
+  pinMode(_output3, OUTPUT);
+  pinMode(_output4, OUTPUT);
+  pinMode(_output5, OUTPUT);
+  pinMode(_output6, OUTPUT);
+  pinMode(_output7, OUTPUT);
+  pinMode(_output8, OUTPUT);
   
 #endif
