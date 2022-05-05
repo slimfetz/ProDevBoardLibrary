6a532d4c8b558c66be26e622f0684fd0888f627d
slimfetz <trentonfetzer_6@hotmail.com>
2022-05-05 15:15:30 -0600

    Update iotBoard.cpp

--- src/iotBoard.cpp
+++ src/iotBoard.cpp
@@ -4,10 +4,46 @@
 iotBoard::iotBoard(char boardType[20]){
   
   if (boardType == _ArduinoMKR){
-    xxxx
+    _input1 = A0;
+    _input2 = A1;
+    _input3 = A2;
+    _input4 = A3;
+    _input5 = A4;
+    _input6 = A5;
+    _input7 = A6;
+    _input8 = 8;
+    _input9 = 9;
+    _input10 = 10;
+  
+    _output1 = 0;
+    _output2 = 1;
+    _output3 = 2;
+    _output4 = 3;
+    _output5 = 4;
+    _output6 = 5;
+    _output7 = 6;
+    _output8 = 7;
   }
   else if (boardType == _ArduinoNano33){
-    xxxx
+    _input1 = A0;
+    _input2 = A1;
+    _input3 = A2;
+    _input4 = A3;
+    _input5 = A4;
+    _input6 = A5;
+    _input7 = A6;
+    _input8 = 8;
+    _input9 = 9;
+    _input10 = 10;
+  
+    _output1 = 0;
+    _output2 = 1;
+    _output3 = 2;
+    _output4 = 3;
+    _output5 = 4;
+    _output6 = 5;
+    _output7 = 6;
+    _output8 = 7;
   }
   
   pinMode(_input1, INPUT);
