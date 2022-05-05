991476951cc88c176d82d0beea897465ec0c9040
slimfetz <trentonfetzer_6@hotmail.com>
2022-05-05 15:13:50 -0600

    Update iotBoard.cpp

--- src/iotBoard.cpp
+++ src/iotBoard.cpp
@@ -9,6 +9,27 @@ iotBoard::iotBoard(char boardType[20]){
   else if (boardType == _ArduinoNano33){
     xxxx
   }
+  
+  pinMode(_input1, INPUT);
+  pinMode(_input2, INPUT);
+  pinMode(_input3, INPUT);
+  pinMode(_input4, INPUT);
+  pinMode(_input5, INPUT);
+  pinMode(_input6, INPUT);
+  pinMode(_input7, INPUT);
+  pinMode(_input8, INPUT);
+  pinMode(_input9, INPUT);
+  pinMode(_input10, INPUT);
+
+  pinMode(_output1, OUTPUT);
+  pinMode(_output2, OUTPUT);
+  pinMode(_output3, OUTPUT);
+  pinMode(_output4, OUTPUT);
+  pinMode(_output5, OUTPUT);
+  pinMode(_output6, OUTPUT);
+  pinMode(_output7, OUTPUT);
+  pinMode(_output8, OUTPUT);
+  pinMode(_output9, OUTPUT);
 }
 
 bool iotBoard::inputDigital(int pinIn){
