434ed211ee8c8f24e325d0b12bb4998c546c8b2a
slimfetz <trentonfetzer_6@hotmail.com>
2022-05-05 15:05:19 -0600

    Update iotBoard.cpp

--- src/iotBoard.cpp
+++ src/iotBoard.cpp
@@ -1,18 +1,57 @@
 
-#include "iotBoard.h"
+#include "iotBoardArduino.h"
 
-iotBoard::iotBoard(String boardType){
+iotBoard::iotBoard(char boardType[20]){
   
-  _boardType = boardType;
-  
+  if (boardType == _ArduinoMKR){
+    xxxx
+  }
+  else if (boardType == _ArduinoNano33){
+    xxxx
+  }
 }
 
 bool iotBoard::inputDigital(int pinIn){
   if (digitalRead(pinIn) == HIGH){
-    digitalReturn = true;
+    _digitalReturn = true;
   }
   else {
-    digitalReturn = false;
+    _digitalReturn = false;
   }
-  return digitalReturn;
+  return _digitalReturn;
+}
+
+void iotBoard::relayOn(int pinOut){
+  switch (pinOut){
+    case 1:
+      digitalWrite(_output1, HIGH);
+    break;
+    case 1:
+      digitalWrite(_output2, HIGH);
+    break;
+    case 1:
+      digitalWrite(_output3, HIGH);
+    break;
+    case 1:
+      digitalWrite(_output4, HIGH);
+    break;
+    case 1:
+      digitalWrite(_output5, HIGH);
+    break;
+    case 1:
+      digitalWrite(_output6, HIGH);
+    break;
+    case 1:
+      digitalWrite(_output7, HIGH);
+    break;
+    case 1:
+      digitalWrite(_output8, HIGH);
+    break;
+    case 1:
+      digitalWrite(_output9, HIGH);
+    break;
+    default:
+    //
+    break;
+  }
 }
